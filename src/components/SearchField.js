92c8302ac4b4ebeb6ca598afe7c4d384a42ee796
Mariana Vazquez <mariana.vazquez7033@gmail.com>
2021-12-15 23:37:49 -0500

    onInputChange works now :)

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -16,45 +16,17 @@ class SearchField extends Component{
     onInputChange= (input) =>{
         this.setState({input});
         console.log(input)
-        fetch('api.openweathermap.org/data/2.5/weather?q='+{input}+'&appid=65f49b185422ab3909aa03d3c7df69de')
-    
+        const apikey = "65f49b185422ab3909aa03d3c7df69de"
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apikey}`)
         // SEARCH+TERM+GOES+HERE&api_key=YOUR_API_KEY
-          .then(response => response.json())
-          .then(data => {
-            this.setState({ found: true})
-          //  console.log(this.state.gifs);
-            console.log("found");
-    
-          })
-          .catch(error => {
-            this.setState({found: false})
-           console.log("error: ")
-         });
-
-        // this.props.onTermChange(input);
+          .then(res => res.json())
+          .then(city => console.log(city))
+        //   .catch(error => {
+        //             this.setState({found: false})
+        //            console.log("error: ", error)
+        //          });
     }
 
-//     componentDidMount() {
-//     fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={65f49b185422ab3909aa03d3c7df69de}")
-//       .then(res => res.json())
-//       .then(
-//         (result) => {
-//           this.setState({
-//             isLoaded: true,
-//             items: result.items
-//           });
-//         },
-//         // Note: it's important to handle errors here
-//         // instead of a catch() block so that we don't swallow
-//         // exceptions from actual bugs in components.
-//         (error) => {
-//           this.setState({
-//             isLoaded: true,
-//             error
-//           });
-//         }
-//       )
-//   }
 
 
 
